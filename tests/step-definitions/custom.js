1bf0e695a19ca31c0e9c8d8af2c9bd82dd75cfb1
Rajab Natshah <33889-RajabNatshah@users.noreply.drupalcode.org>
2026-07-25 11:15:06 +0000

    ci: #3613157 Add webship-js CI test and Reviewed-by-human .gitlab checkpoints

---
+++ tests/step-definitions/custom.js
@@ -0,0 +1,4 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const playwrightConfig = require(require('path').join(process.cwd(), 'playwright.config'));
+
+// Add your custom step definitions here.
